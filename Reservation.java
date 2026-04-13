484797fd656ef9c4995f83723e3b5ccbd6f677f9
ebraheemothman <ebraheemosman10@gmail.com>
2026-04-13 21:54:23 +0200

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -8,6 +8,38 @@ package com.mycompany.oop_project;
  *
  * @author ahmad
  */
-public class Reservation {
-    
+ class Reservation { /*task member 3 */
+    private int Days ;
+    private Guest guest =new Guest(); 
+    private Room room =new Room();   
+    private LocalDate checkInDay;
+    private LocalDate checkOutDay;
+   public int getDays(LocalDate S ,LocalDate E ) {
+    int startDay = S.getDayOfMonth(); 
+    int endDay = E.getDayOfMonth();
+    return endDay-startDay;
+    /*incomplete */
+   }
+   
+    private ReservationStatue status;
+    public Reservation(Guest guest, Room room, LocalDate checkIn, LocalDate checkOut) {
+        this.guest = guest;
+        this.room = room;
+        this.checkInDay = checkIn;
+        this.checkOutDay = checkOut;
+        this.status = ReservationStatue.PENDING; 
+        Days = getDays(checkIn,checkOut);
+    }
+    private enum ReservationStatue{
+        PENDING,
+        CONFIRMED,
+        CANCELLED,
+        COMPLETED;
+    }
+    public Guest getGuest() { return guest; }
+    public Room getRoom() { return room; }
+    public LocalDate getCheckInDay() { return checkInDay; }
+    public LocalDate getCheckOutDay() { return checkOutDay; }
+    public ReservationStatue getStatus() { return status; }
+    public void setStatus(ReservationStatue status) { this.status = status; }
 }
