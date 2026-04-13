40ee4f34f7a09c9d76a9d8e49ec171158b89f595
ebraheemothman <ebraheemosman10@gmail.com>
2026-04-13 21:55:41 +0200

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -8,6 +8,23 @@ package com.mycompany.oop_project;
  *
  * @author ahmad
  */
-public class Invoice {
-    
-}
+class Invoice  {
+   private  enum PaymentMethod{
+       cash,
+       credit_card,
+       online;
+   }
+   
+   private final LocalDate paymentDate =localDate.now();
+   private Reservation reservation = new Reservation() ; /*not finished*/
+   Invoice (Reservation reservation)
+   {
+       this.reservation= reservation ;
+   }
+  public printInvoce (){
+      System.out.println ("||=========Invoice=========||");
+      System.out.println ("||guest name :" + reservation.guest.name + "   ||");
+      System.out.println (" total price = " + reservation.room.pricePerNight*reservation.Days + "EGP" );
+      System.out.println("Payment Method: " + paymentMethod); /*same here */
+      System.out.println("Date: " + paymentDate);
+  }
