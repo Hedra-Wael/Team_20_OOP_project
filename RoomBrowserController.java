7a04d89db3aa8aa0a25d214643068cd8f916166a
ebraheemothman <ebraheemosman10@gmail.com>
2026-05-09 02:17:20 +0300

    Update RoomBrowserController.java

--- RoomBrowserController.java
+++ RoomBrowserController.java
@@ -1,17 +1,20 @@
 package com.mycompany.oop_project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ComboBox;
+import java.io.IOException;
 
 public class RoomBrowserController {
 
     @FXML private ListView<Room> roomListView;
     @FXML private ComboBox<String> roomTypeFilter;
 
+    private SceneController sceneController = new SceneController();
+
     @FXML
     public void initialize() {
-        // Direct population from the Milestone 1 static list 
         roomListView.getItems().setAll(HotelDatabase.rooms);
     }
 
@@ -20,11 +23,15 @@ public class RoomBrowserController {
         String selectedType = roomTypeFilter.getValue();
         roomListView.getItems().clear();
 
-        // Simple loop to check each room
         for (Room r : HotelDatabase.rooms) {
             if (selectedType == null || selectedType.equals("All") || r.getRoomType().getName().equalsIgnoreCase(selectedType)) {
                 roomListView.getItems().add(r);
             }
         }
+    }
+
+    @FXML
+    public void goBack(ActionEvent event) throws IOException {
+        sceneController.switchToDashboard(event);
     }
 }
