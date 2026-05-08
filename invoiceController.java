c49be4efed146d2525d42de8afdea61a15b56ded
ebraheemothman <ebraheemosman10@gmail.com>
2026-05-08 08:40:40 +0300

    Add files via upload

---
+++ invoiceController.java
@@ -0,0 +1,59 @@
+package com.example.test;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class invoiceController implements Initializable {
+
+    @FXML private Label guestNameLabel;
+    @FXML private Label totalAmountLabel;
+    @FXML private Label statusLabel;
+    @FXML private ChoiceBox<PaymentMethod> choiceBox;
+
+    private Reservation currentReservation;
+    private Invoice currentInvoice;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choiceBox.getItems().addAll(PaymentMethod.values());
+    }
+
+    public void setupInvoice(Reservation reservation) {
+        this.currentReservation = reservation;
+
+        double pricePerNight = reservation.getRoom().getRoomType().getBasePrice();
+
+        currentInvoice = new Invoice(HotelDatabase.invoices.size() + 5001, reservation, pricePerNight, 0.14);
+
+        guestNameLabel.setText(reservation.getGuest().getUsername());
+        totalAmountLabel.setText(String.format("%.2f EGP", currentInvoice.getTotalAmount()));
+    }
+
+    @FXML
+    public void payNow(ActionEvent e) {
+        PaymentMethod selectedMethod = choiceBox.getValue();
+
+        if (selectedMethod == null) {
+            statusLabel.setText("Please select a payment method!");
+            return;
+        }
+
+        try {
+            currentInvoice.processPayment(selectedMethod);
+
+            statusLabel.setText("Success! New Balance: " + currentReservation.getGuest().getBalance());
+            statusLabel.setStyle("-fx-text-fill: green;");
+
+            HotelDatabase.invoices.add(currentInvoice);
+
+        } catch (InvalidPaymentException ex) {
+            statusLabel.setText(ex.getMessage());
+            statusLabel.setStyle("-fx-text-fill: red;");
+        }
+    }
+}
