c1f0999568dc379979ff3ea391936aa169d45114
ebraheemothman <ebraheemosman10@gmail.com>
2026-05-09 07:17:24 +0300

    Update Amenity.java

--- Amenity.java
+++ Amenity.java
@@ -1,34 +1,8 @@
-import java.util.ArrayList;
-import java.time.LocalDate;
-class Amenity {
-    private String name;
+package com.mycompany.oop_project;
 
+public class Amenity {
+    private String name;
     public Amenity(String name) {
-        this.name = name;
+        this.name = name; 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//da gemini edahooly ka t3deel 3ala 2l Amenity 3alashan 2l GUI
-
-//public class Amenity { // Added 'public'
-//    private String name;
-//
-//    public Amenity(String name) {
-//        this.name = name;
-//    }
-//    
-//    public String getName() { return name; } // Added Getter
-//}
